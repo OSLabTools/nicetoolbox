fdfc8b4ccc52a730b04f13c22c3a875f8cd64ef2
Timo Luebbing <timo.luebbing@tuebingen.mpg.de>
2024-11-29 10:57:26 +0100

    update main for streamlit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import subprocess
 
 from nicetoolbox.detectors.main import main as detectors_main
 from nicetoolbox.evaluation.main import main as evaluation_main
@@ -26,23 +27,32 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     working_directory = os.getcwd()
+    machine_specifics = f"{working_directory}/machine_specific_paths.toml"
 
     if args.run == "detectors":
         run_config = f"{working_directory}/nicetoolbox/detectors/configs/run_file.toml"
-        machine_specifics = f"{working_directory}/machine_specific_paths.toml"
         detectors_main(run_config, machine_specifics)
         
     elif args.run == "evaluation":
         eval_config = f"{working_directory}/nicetoolbox/evaluation/configs/evaluation_config.toml"
-        machine_specifics = f"{working_directory}/machine_specific_paths.toml"
         evaluation_main(eval_config, machine_specifics)
         
     elif args.run == "visual_media":
         visual_media_main()
 
     elif args.run == "visual_stats":
-        #! add running streamlit
-        pass
+    
+        main = "main_statistics.py"
+        script_command = f"streamlit run {main}"
+        venv_path = "envs/nicetoolbox/bin/activate"
+        command = f"source {venv_path} && {script_command}"
+        
+        subprocess.run(
+            command, 
+            text=True,
+            shell=True,
+            executable="/bin/bash"
+        )
 
     elif args.run == "calibration_converter":
         calibration_converter()
