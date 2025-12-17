cf188c25e21936bd9f95bf397f96d429f9fd9094
Aleksandr Evgrashin <aleksandr.evgrashin@tuebingen.mpg.de>
2025-12-17 12:58:49 +0100

    Bringing back CI/CD

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 TAG=${1:-"latest"}
+DEV=${2:-false}
 
 GIT_HASH=$(git rev-parse HEAD)
 GIT_SUMMARY=$(git log -1 --pretty=%s)
@@ -8,6 +9,7 @@ GIT_SUMMARY=$(git log -1 --pretty=%s)
 echo "Building Docker image, tag: $TAG"
 echo "Building with commit: $GIT_HASH"
 echo "Commit message: $GIT_SUMMARY"
+echo "Dev mode: $DEV"
 
 # remove current nicetoolbox to avoid dangling images
 docker rmi mpioslab/nicetoolbox:$TAG 2>/dev/null || true
@@ -16,4 +18,5 @@ docker rmi mpioslab/nicetoolbox:$TAG 2>/dev/null || true
 docker build \
   --build-arg NICETOOLBOX_GIT_HASH="$GIT_HASH" \
   --build-arg NICETOOLBOX_GIT_SUMMARY="$GIT_SUMMARY" \
+  --build-arg NICETOOLBOX_DEV="$DEV" \
   -t mpioslab/nicetoolbox:$TAG .
